39053631dce3ef84012eafff8194953f4adc7b18
unknown <jerryshin961115@gmail.com>
2019-07-04 17:56:07 +0800

    開始實作dashboard

--- react-project-model/src/views/Article/Edit.js
+++ react-project-model/src/views/Article/Edit.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Form, Icon, Input, Button } from 'antd';
 
-export default class Edit extends Component {
-    constructor(props){
-        super()
-    }
-    render() {
-        console.log("props",this.props)
-        return (
-            <div>
-               
-            </div>
-        )
-    }
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
 }
+
+class EditPage extends React.Component {
+  componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields();
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.props.location)
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+    // Only show error after a field is touched.
+    const usernameError = isFieldTouched('username') && getFieldError('username');
+    return (
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <Form.Item validateStatus={usernameError ? 'error' : ''} help={usernameError || ''}>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const Edit = Form.create({ name: 'horizontal_login' })(EditPage);
+
+export default Edit
--- react-project-model/src/views/Dashboard/index.js
+++ react-project-model/src/views/Dashboard/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import { Card, Row, Col } from 'antd';
 
 export default class Dashboard
- extends Component {
+    extends Component {
     render() {
         return (
             <div>
-                这里是仪表盘
+                <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: "100%" }}>
+                    <div>
+                        <Row>
+                            <Col span={12}>col-12</Col>
+                            <Col span={12}>col-12</Col>
+                        </Row>
+                        <Row>
+                            <Col span={8}>col-8</Col>
+                            <Col span={8}>col-8</Col>
+                            <Col span={8}>col-8</Col>
+                        </Row>
+                        <Row>
+                            <Col span={6}>col-6</Col>
+                            <Col span={6}>col-6</Col>
+                            <Col span={6}>col-6</Col>
+                            <Col span={6}>col-6</Col>
+                        </Row>
+                    </div>
+                </Card>
             </div>
         )
     }
